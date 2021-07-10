91b853169e20145566d65c2cfed0cad933f2db0d
nataliyah123 <nataliyah123@users.noreply.github.com>
2021-07-10 19:21:28 +0500

    q2

---
+++ median.js
@@ -0,0 +1,13 @@
+let x = [2,3,4,5,7]
+let y =[3,5,7,9,11]
+
+let xlen = x.length
+let ylen  = y.length
+
+let mid1 = Math.floor(xlen/2)
+let mid2 = Math.floor(ylen/2)
+
+let k = x[mid1]
+let l = y[mid2]
+
+console.log('ibia',k, l)
